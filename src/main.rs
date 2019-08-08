0f73f77e5fe1f3fca40511933711efdb90cf30ff
Shitong Wen <shitwen@qti.qualcomm.com>
2019-08-08 17:28:14 +0800

    remove termion to run on win

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,37 @@
-extern crate time;
-extern crate sha1;
 extern crate bincode;
-extern crate termion;
+extern crate sha1;
+extern crate time;
 
 extern crate serde;
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 
-mod hash_content;
 mod block;
 mod blocks;
-mod peers;
-mod help;
 mod display;
+mod hash_content;
+mod help;
 mod message;
+mod peers;
 
 use std::io::Read;
 use std::net::TcpListener;
+use std::sync::{Arc, Mutex};
 use std::thread::spawn;
-use std::sync::{
-    Arc,
-    Mutex,
-};
 
 use bincode::deserialize;
 
 use block::Block;
 
-use blocks::{
-    list_blocks,
-    broadcast_block,
-    add_block_from_message,
-    send_last_block_to_stream,
-};
+use blocks::{add_block_from_message, broadcast_block, list_blocks, send_last_block_to_stream};
 
-use peers::{
-    get_chain_from_stream,
-    list_peers,
-    create_stream,
-};
+use peers::{create_stream, get_chain_from_stream, list_peers};
 
 use help::list_commands;
 
-use display::{
-    clear_screen,
-    get_input,
-    set_cursor_into_logs,
-    set_cursor_into_input,
-};
+use display::{clear_screen, get_input, set_cursor_into_input, set_cursor_into_logs};
 
-use message::{
-    Message,
-    MessageLabel,
-};
+use message::{Message, MessageLabel};
 
 const LISTENING_PORT: &str = "10000";
 
@@ -61,17 +41,15 @@ const LISTENING_PORT: &str = "10000";
 ///
 /// `chain` - the chain to manipulate
 fn handle_incoming_connections(chain: Arc<Mutex<Vec<Block>>>) {
-
     let address = format!("0.0.0.0:{}", LISTENING_PORT);
     let listener = TcpListener::bind(address).unwrap();
 
     /* blocks until data is received */
     for income in listener.incoming() {
-
         /* TODO: display message when receive a connection;
-           should use mutex as it must modify the content
-           of the main text area (so the cursor position
-           must not be modified) */
+        should use mutex as it must modify the content
+        of the main text area (so the cursor position
+        must not be modified) */
 
         clear_screen();
         set_cursor_into_logs();
@@ -82,22 +60,17 @@ fn handle_incoming_connections(chain: Arc<Mutex<Vec<Block>>>) {
         let mut buffer: Vec<u8> = vec![0; MESSAGE_MAX_LENGTH];
 
         /* blocks until data is received  */
-        stream.read(&mut buffer).expect("Received message is too long.");
+        stream
+            .read(&mut buffer)
+            .expect("Received message is too long.");
 
         let message: Message = deserialize(&buffer).unwrap();
         let label = message.get_label();
 
         if label == &MessageLabel::AskForAllBlocks {
-            send_last_block_to_stream(
-                stream,
-                &chain,
-            );
-        }
-        else if label == &MessageLabel::SendBlock {
-            add_block_from_message(
-                &chain,
-                &message,
-            );
+            send_last_block_to_stream(stream, &chain);
+        } else if label == &MessageLabel::SendBlock {
+            add_block_from_message(&chain, &message);
         }
 
         set_cursor_into_input();
@@ -105,7 +78,6 @@ fn handle_incoming_connections(chain: Arc<Mutex<Vec<Block>>>) {
 }
 
 fn main() {
-
     clear_screen();
 
     println!("Type 'help' to list commands.");
@@ -114,18 +86,19 @@ fn main() {
     let mut peers: Vec<String> = Vec::new();
 
     let listener_chain = chain.clone();
-    spawn(|| { handle_incoming_connections(listener_chain) });
+    spawn(|| handle_incoming_connections(listener_chain));
 
     loop {
-
         let input = get_input();
         let splitted: Vec<&str> = input.split(' ').collect();
 
         /* get() returns &&str, so we mention result type &str
-           and get it from a reference (*) */
+        and get it from a reference (*) */
         let command: &str = match splitted.get(0) {
             Some(value) => *value,
-            None => { continue; }
+            None => {
+                continue;
+            }
         };
 
         const ADD_BLOCK: &str = "add_block";
@@ -138,9 +111,7 @@ fn main() {
         let option = match splitted.get(1) {
             Some(option) => option,
             None => {
-
-                if command == ADD_BLOCK ||
-                    command == ADD_PEER {
+                if command == ADD_BLOCK || command == ADD_PEER {
                     continue;
                 }
 
@@ -149,18 +120,13 @@ fn main() {
         };
 
         if command == ADD_BLOCK {
-
             let data: i32 = option.parse().unwrap();
             let mut chain = chain.lock().unwrap();
 
             let mut previous_digest = String::new();
 
             if !chain.is_empty() {
-
-                previous_digest = chain.last()
-                    .unwrap()
-                    .get_current()
-                    .to_string();
+                previous_digest = chain.last().unwrap().get_current().to_string();
             }
 
             let block = Block::new(data, previous_digest);
@@ -169,12 +135,9 @@ fn main() {
             println!("New block added.");
 
             broadcast_block(&peers, block);
-        }
-        else if command == SEE_BLOCKCHAIN {
+        } else if command == SEE_BLOCKCHAIN {
             list_blocks(&chain);
-        }
-        else if command == ADD_PEER {
-
+        } else if command == ADD_PEER {
             let full_address = format!("{}:{}", option, LISTENING_PORT);
             peers.push(full_address.clone());
 
@@ -193,14 +156,11 @@ fn main() {
                     println!("The local chain is up-to-date compared to the remote one.");
                 }
             }
-        }
-        else if command == LIST_PEERS {
+        } else if command == LIST_PEERS {
             list_peers(&peers);
-        }
-        else if command == HELP {
+        } else if command == HELP {
             list_commands();
-        }
-        else if command == EXIT {
+        } else if command == EXIT {
             break;
         } else {
             println!("Command not found. Type 'help' to list commands.");
